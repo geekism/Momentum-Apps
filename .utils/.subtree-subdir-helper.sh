da35fc85ac637e7438100af775a303263f8eb494
Willy-JL <49810075+Willy-JL@users.noreply.github.com>
2024-10-05 01:05:56 +0100

    Revert "Nevermind, use git's subtree split --rejoin method"
    This reverts commit a96e8734ec75c95a980214fdebfc6f201a82afcf.

--- .utils/.subtree-subdir-helper.sh
+++ .utils/.subtree-subdir-helper.sh
@@ -16,30 +16,33 @@ action="${5}"
 prevbranch="$(git branch --show-current)"
 temp="$(rev <<< "${repo%/}" | cut -d/ -f1,2 | rev | tr / -)-$(tr / - <<< "${branch}")"
 fetch="_fetch-${temp}"
-cache="_cache-${temp}"
 split="_split-${temp}-$(tr / - <<< "${subdir}")"
 git fetch --no-tags "${repo}" "${branch}:${fetch}"
-if git rev-parse --verify "${cache}" &> /dev/null; then
-    git checkout "${cache}"
-    git merge "${fetch}"
+merged="$(git log --merges -1 --format=%P --grep=" ${path} from ${repo}" -- "${path}" | cut -d' ' -f2)"
+hash="$(git rev-parse ${fetch})"
+skip=false
+if git diff --quiet "${merged}" "${hash}" -- "${subdir}"; then
+    skip=true
+fi
+if $skip; then
+    echo "No updates, skipping expensive subtree split."
 else
-    git checkout -b "${cache}" "${fetch}"
-fi
-
-ok=true
-exec {capture}>&1
-result="$(git subtree split -P "${subdir}" --rejoin -b "${split}" 2>&1 | tee /proc/self/fd/$capture)"
-if grep "is not an ancestor of commit" <<< "$result" > /dev/null; then
-    echo "Resetting split branch..."
-    git branch -D "${split}"
-    git subtree split -P "${subdir}" -b "${split}"
-fi
-if grep "^fatal: " <<< "$result" > /dev/null; then
-    ok=false
-fi
-git checkout "${prevbranch}"
-if $ok; then
+    ok=true
+    git checkout "${fetch}"
     exec {capture}>&1
-    result="$(git subtree "${action}" -P "${path}" "${split}" -m "${action^} ${path} from ${repo}" 2>&1 | tee /proc/self/fd/$capture)"
-    bash .utils/.check-merge.sh "${path}" "${repo}" "${result}"
+    result="$(git subtree split -P "${subdir}" -b "${split}" 2>&1 | tee /proc/self/fd/$capture)"
+    if grep "is not an ancestor of commit" <<< "$result" > /dev/null; then
+        echo "Resetting split branch..."
+        git branch -D "${split}"
+        git subtree split -P "${subdir}" -b "${split}"
+    fi
+    if grep "^fatal: " <<< "$result" > /dev/null; then
+        ok=false
+    fi
+    git checkout "${prevbranch}"
+    if $ok; then
+        exec {capture}>&1
+        result="$(git subtree "${action}" -P "${path}" "${split}" -m "${action^} ${path} from ${repo}" 2>&1 | tee /proc/self/fd/$capture)"
+        bash .utils/.check-merge.sh "${path}" "${repo}" "${result}"
+    fi
 fi
